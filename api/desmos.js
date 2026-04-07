ab60c56bdf13911e989ffe3a2b6d6a3350ac2a9f
Shreyash Tijare <81917827+shreyashtijare@users.noreply.github.com>
2026-04-07 23:25:26 +0530

    Refactor Desmos API script loading
    Refactor the handler function to use an IIFE for script loading and set the script to load asynchronously.

--- api/desmos.js
+++ api/desmos.js
@@ -1,11 +1,13 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const key = process.env.DESMOS_KEY;
 
   res.setHeader("Content-Type", "application/javascript");
-  res.send(`
-    window.DESMOS_API_KEY = "${key}";
-    const s = document.createElement("script");
-    s.src = "https://www.desmos.com/api/v1.11/calculator.js?apiKey=${key}";
-    document.head.appendChild(s);
+  res.status(200).send(`
+    (function() {
+      var s = document.createElement("script");
+      s.src = "https://www.desmos.com/api/v1.11/calculator.js?apiKey=${key}";
+      s.async = true;
+      document.head.appendChild(s);
+    })();
   `);
 }
